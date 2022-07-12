3a4ea04a3b9c7e135133b9dea6fceea8d09d0c22
SahManish21 <manish.sahoo2003@gmail.com>
2022-07-12 21:30:15 +0530

    First commit

---
+++ do while.py
@@ -0,0 +1,9 @@
+n=98
+x=int(input("Enter a positive number less than hundred"))
+while True:
+    if(x==n):
+        print("end of loop")
+        break
+    else:
+        print("0")
+        x=x+1
---
+++ evenodd.py
@@ -0,0 +1,5 @@
+x = int(input("Please enter amount"))
+if x % 2 == 0:
+    print('even number')
+else:
+    print('odd number')
